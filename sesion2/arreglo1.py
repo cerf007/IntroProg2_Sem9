845c69789de6a47a07a14e7f0005da01cf510999
cerf007 <cerf02062007@gmail.com>
2025-05-20 11:26:33 -0600

    Arreglo de listas manuales

---
+++ sesion2/arreglo1.py
@@ -0,0 +1,5 @@
+#. Cargar una lista manualmente
+# Crea una lista con 10 números enteros y muestra el contenido.
+
+array_int = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f"Lista de numeros enteros: {array_int}")
