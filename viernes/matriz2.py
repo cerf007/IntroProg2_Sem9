d22c85265d6509a9b4b91fb90fb11ba5542a1000
cerf007 <cerf02062007@gmail.com>
2025-05-23 07:41:21 -0600

    Se agrego limitación decimal

---
+++ viernes/matriz2.py
@@ -0,0 +1,9 @@
+#Matriz de numero flotantes
+
+matriz = [[10.6, 2.8], [300.98, 4.25]]
+print("_"*17)
+for fila in matriz:
+    for columna in fila:
+        print(f"|{columna:>6.1f}", end= " ")
+    print("|")
+    print("_"*17)
