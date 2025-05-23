1d56f6d9ebe4f347dd672d73d7340389d1a1cc8e
cerf007 <cerf02062007@gmail.com>
2025-05-23 07:32:24 -0600

    Matriz mejorada

--- viernes/matriz1.py
+++ viernes/matriz1.py
@@ -1,6 +1,8 @@
 #MATRIZ
 matriz = [[10,2], [300,4]]
+print("_"*17)
 for fila in matriz:
     for columna in fila:
-        print(f"{columna:>6}", end= " ")
-    print()
+        print(f"|{columna:>6}", end= " ")
+    print("|")
+    print("_"*17)
